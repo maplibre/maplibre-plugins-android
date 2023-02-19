fbcc7cea91b3ffdd940b80d4a558c110c24a7570
Victor Temprano <tempranova@gmail.com>
2023-02-19 10:48:35 -0800

    testapp ref updates

---
+++ app/src/androidTest/java/org/maplibre/android/plugins/utils/OnMapReadyIdlingResource.java
@@ -0,0 +1,63 @@
+package org.maplibre.android.plugins.utils;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import org.maplibre.android.plugins.testapp.R;
+
+import androidx.annotation.NonNull;
+import androidx.test.espresso.IdlingResource;
+
+public class OnMapReadyIdlingResource implements IdlingResource, OnMapReadyCallback {
+
+  private MapboxMap mapboxMap;
+  private MapView mapView;
+  private IdlingResource.ResourceCallback resourceCallback;
+
+  public OnMapReadyIdlingResource(Activity activity) {
+    new Handler(Looper.getMainLooper()).post(() -> {
+      mapView = activity.findViewById(R.id.mapView);
+      if (mapView != null) {
+        mapView.getMapAsync(OnMapReadyIdlingResource.this);
+      }
+    });
+  }
+
+  @Override
+  public String getName() {
+    return getClass().getSimpleName();
+  }
+
+  @Override
+  public boolean isIdleNow() {
+    return mapboxMap != null && mapboxMap.getStyle() != null && mapboxMap.getStyle().isFullyLoaded();
+  }
+
+  @Override
+  public void registerIdleTransitionCallback(ResourceCallback resourceCallback) {
+    this.resourceCallback = resourceCallback;
+  }
+
+  public MapView getMapView() {
+    return mapView;
+  }
+
+  public MapboxMap getMapboxMap() {
+    return mapboxMap;
+  }
+
+  @Override
+  public void onMapReady(@NonNull MapboxMap mapboxMap) {
+    this.mapboxMap = mapboxMap;
+    mapboxMap.setStyle(Style.MAPBOX_STREETS, style -> {
+      if (resourceCallback != null) {
+        resourceCallback.onTransitionToIdle();
+      }
+    });
+  }
+}
