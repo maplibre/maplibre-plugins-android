615861fb27ab5c1ceb45370a34fa1cdc0dbf316c
Jonas Vautherin <jonas.vautherin@gmail.com>
2023-06-21 20:17:19 +0200

    ci: fix build (#34)
    * ci: set Java version to 11
    
    * Fix style errors after indentation was changed almost everywhere
    
    * Fix ktlintCheck

--- app/src/debug/java/com/mapbox/mapboxsdk/plugins/testapp/activity/SingleFragmentActivity.java
+++ app/src/debug/java/com/mapbox/mapboxsdk/plugins/testapp/activity/SingleFragmentActivity.java
@@ -14,24 +14,24 @@ import androidx.fragment.app.Fragment;
  * Used for testing fragments inside a fake activity.
  */
 public class SingleFragmentActivity extends AppCompatActivity {
-  @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    FrameLayout content = new FrameLayout(this);
-    content.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-      ViewGroup.LayoutParams.MATCH_PARENT));
-    content.setId(R.id.container);
-    setContentView(content);
-  }
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FrameLayout content = new FrameLayout(this);
+        content.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT));
+        content.setId(R.id.container);
+        setContentView(content);
+    }
 
-  public void setFragment(Fragment fragment) {
-    getSupportFragmentManager().beginTransaction()
-      .add(R.id.container, fragment, "TEST")
-      .commit();
-  }
+    public void setFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+            .add(R.id.container, fragment, "TEST")
+            .commit();
+    }
 
-  public void replaceFragment(Fragment fragment) {
-    getSupportFragmentManager().beginTransaction()
-      .replace(R.id.container, fragment).commit();
-  }
+    public void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+            .replace(R.id.container, fragment).commit();
+    }
 }
