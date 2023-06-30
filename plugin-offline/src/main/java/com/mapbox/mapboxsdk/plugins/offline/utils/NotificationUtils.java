8e8ea5ce09abcadaef15fc7160131ffba1dcd271
Andrew Holbrook <atholbro@gmail.com>
2023-06-30 14:51:54 -0400

    Offline: fix crash on Android S+ caused by pending intent.

--- plugin-offline/src/main/java/com/mapbox/mapboxsdk/plugins/offline/utils/NotificationUtils.java
+++ plugin-offline/src/main/java/com/mapbox/mapboxsdk/plugins/offline/utils/NotificationUtils.java
@@ -52,6 +52,6 @@ public class NotificationUtils {
             .addAction(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP ? 0 : R.drawable.ic_cancel,
                 options.cancelText(),
                 PendingIntent.getService(context, offlineDownload.uuid().intValue(), cancelIntent,
-                    PendingIntent.FLAG_CANCEL_CURRENT));
+                    PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_IMMUTABLE));
     }
 }
