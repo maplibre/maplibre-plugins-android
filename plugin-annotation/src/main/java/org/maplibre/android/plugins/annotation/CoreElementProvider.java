4ab9228f70bd86055f78ba25572581f933fb44e6
kocheicks <theshevapro@gmail.com>
2024-05-14 13:46:03 -0400

    updated package import paths to maplibre

---
+++ plugin-annotation/src/main/java/org/maplibre/android/plugins/annotation/CoreElementProvider.java
@@ -0,0 +1,17 @@
+package org.maplibre.android.plugins.annotation;
+
+import org.maplibre.android.style.layers.Layer;
+import org.maplibre.android.style.sources.GeoJsonSource;
+import org.maplibre.android.style.sources.GeoJsonOptions;
+import androidx.annotation.Nullable;
+
+interface CoreElementProvider<L extends Layer> {
+
+    String getLayerId();
+
+    String getSourceId();
+
+    L getLayer();
+
+    GeoJsonSource getSource(@Nullable GeoJsonOptions geoJsonOptions);
+}
---
+++ plugin-annotation/src/main/java/org/maplibre/android/plugins/annotation/OnAnnotationLongClickListener.java
@@ -0,0 +1,18 @@
+package org.maplibre.android.plugins.annotation;
+
+/**
+ * Generic interface definition of a callback to be invoked when an annotation has been long clicked.
+ *
+ * @param <T> generic parameter extending from Annotation
+ */
+public interface OnAnnotationLongClickListener<T extends Annotation> {
+
+    /**
+     * Called when an annotation has been long clicked
+     *
+     * @param t the annotation long clicked.
+     * @return True if this click should be consumed and not passed further to other listeners
+     * registered afterwards, false otherwise.
+     */
+    boolean onAnnotationLongClick(T t);
+}
