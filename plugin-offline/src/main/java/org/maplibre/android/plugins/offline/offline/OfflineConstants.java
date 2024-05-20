5989cc9531a2e403acda10430dcda25629994fa2
Bart Louwers <bart.louwers@gmail.com>
2024-05-20 14:44:28 +0200

    Replace constants

--- plugin-offline/src/main/java/org/maplibre/android/plugins/offline/offline/OfflineConstants.java
+++ plugin-offline/src/main/java/org/maplibre/android/plugins/offline/offline/OfflineConstants.java
@@ -6,23 +6,23 @@ public class OfflineConstants {
         // No Instances
     }
 
-    static final String ACTION_START_DOWNLOAD = "com.mapbox.mapboxsdk.plugins.offline.download.start";
-    static final String ACTION_CANCEL_DOWNLOAD = "com.mapbox.mapboxsdk.plugins.offline.download.cancel";
-    static final String ACTION_OFFLINE = "com.mapbox.mapboxsdk.plugins.offline";
-    static final String KEY_STATE = "com.mapbox.mapboxsdk.plugins.offline.state";
-    static final String STATE_STARTED = "com.mapbox.mapboxsdk.plugins.offline.state.started";
-    static final String STATE_FINISHED = "com.mapbox.mapboxsdk.plugins.offline.state.complete";
-    static final String STATE_ERROR = "com.mapbox.mapboxsdk.plugins.offline.state.error";
-    static final String STATE_CANCEL = "com.mapbox.mapboxsdk.plugins.offline.state.cancel";
-    static final String STATE_PROGRESS = "com.mapbox.mapboxsdk.plugins.offline.state.progress";
-    static final String KEY_BUNDLE_OFFLINE_REGION = "com.mapbox.mapboxsdk.plugins.offline.region";
-    static final String KEY_BUNDLE_ERROR = "com.mapbox.mapboxsdk.plugins.offline.error";
-    static final String KEY_BUNDLE_MESSAGE = "com.mapbox.mapboxsdk.plugins.offline.error";
-    static final String KEY_PROGRESS = "com.mapbox.mapboxsdk.plugins.offline.progress";
+    static final String ACTION_START_DOWNLOAD = "org.maplibre.android.plugins.offline.download.start";
+    static final String ACTION_CANCEL_DOWNLOAD = "org.maplibre.android.plugins.offline.download.cancel";
+    static final String ACTION_OFFLINE = "org.maplibre.android.plugins.offline";
+    static final String KEY_STATE = "org.maplibre.android.plugins.offline.state";
+    static final String STATE_STARTED = "org.maplibre.android.plugins.offline.state.started";
+    static final String STATE_FINISHED = "org.maplibre.android.plugins.offline.state.complete";
+    static final String STATE_ERROR = "org.maplibre.android.plugins.offline.state.error";
+    static final String STATE_CANCEL = "org.maplibre.android.plugins.offline.state.cancel";
+    static final String STATE_PROGRESS = "org.maplibre.android.plugins.offline.state.progress";
+    static final String KEY_BUNDLE_OFFLINE_REGION = "org.maplibre.android.plugins.offline.region";
+    static final String KEY_BUNDLE_ERROR = "org.maplibre.android.plugins.offline.error";
+    static final String KEY_BUNDLE_MESSAGE = "org.maplibre.android.plugins.offline.error";
+    static final String KEY_PROGRESS = "org.maplibre.android.plugins.offline.progress";
     public static final String NOTIFICATION_CHANNEL = "offline";
 
-    public static final String RETURNING_DEFINITION = "com.mapbox.mapboxsdk.plugins.offline.returning.definition";
-    public static final String RETURNING_REGION_NAME = "com.mapbox.mapboxsdk.plugins.offline.returing.region.name";
+    public static final String RETURNING_DEFINITION = "org.maplibre.android.plugins.offline.returning.definition";
+    public static final String RETURNING_REGION_NAME = "org.maplibre.android.plugins.offline.returing.region.name";
 
-    public static final String KEY_BUNDLE = "com.mapbox.mapboxsdk.plugins.offline.download.object";
+    public static final String KEY_BUNDLE = "org.maplibre.android.plugins.offline.download.object";
 }
