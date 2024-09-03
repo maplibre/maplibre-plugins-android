59f8c02e44821773ee447cf3349920b6c8c51635
Bart Louwers <bart.louwers@gmail.com>
2024-09-03 15:01:59 +0200

    Update AGP, Kotlin

--- app/src/main/java/org/maplibre/android/plugins/testapp/PluginApplication.kt
+++ app/src/main/java/org/maplibre/android/plugins/testapp/PluginApplication.kt
@@ -1,7 +1,6 @@
 package org.maplibre.android.plugins.testapp
 
 import android.app.Application
-import com.squareup.leakcanary.LeakCanary
 import org.maplibre.android.MapLibre
 import timber.log.Timber
 
@@ -9,11 +8,6 @@ class PluginApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            return
-        }
-
-        LeakCanary.install(this)
         initializeLogger()
         MapLibre.getInstance(this)
     }
